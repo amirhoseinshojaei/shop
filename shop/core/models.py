18fa52421d381d733c735e3fe33bddfa55fad474
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-11-06 00:26:37 +0330

    build products model

--- shop/core/models.py
+++ shop/core/models.py
@@ -10,6 +10,13 @@ from slugify import slugify
 # Create your models here.
 
 
+# Status choices
+STATUS_CHOICES = [
+    ('in_stock','InStock'),
+    ('out_of_stock', 'Out Of Stock'),
+]
+
+
 class MyAccountManager(BaseUserManager):
     def create_user(self, email, phone_number , password=None):
         if not email:
@@ -147,6 +154,40 @@ class Suppliers(models.Model):
 
 
 
+# All of Products
+class Products(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    category = models.ForeignKey(Categories, on_delete= models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=250, choices=STATUS_CHOICES, default='in_stock')
+    is_suggestion = models.BooleanField(default=False , null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Products, self).save(*args, **kwargs)
+
+    
+    def calculate_discount_percentage(self):
+        if self.is_sale and self.sale_price < self.price:
+            discount = (self.price - self.sale_price) / self.price * 100
+            return round(discount, 0)
+        
+
+    def __str__(self):
+        return self.name
+
+    
 
 
 
