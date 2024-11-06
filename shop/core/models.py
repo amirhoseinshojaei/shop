9445a1e94c13cb6fc3ad3ebd388e0107d8ca31c4
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-11-06 23:02:17 +0330

    build a model for OrderItems

--- shop/core/models.py
+++ shop/core/models.py
@@ -228,6 +228,23 @@ def set_update_shipped_on_updated(sender,instance,**kwargs):
             instance.date_shipped = now
 
 
+# OrderItem Model
+class OrderItems(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    order = models.ForeignKey(Orders, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    product = models.ForeignKey(Products,on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        verbose_name = 'OrderItem'
+        verbose_name_plural = 'OrderItems'
+
+    def __str__(self):
+        return self.id
+    
+
 
 
 
