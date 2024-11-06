490a4eb0e8cd656d431221d27fb5523bb3f29977
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-11-07 00:38:29 +0330

    register product model in panel admin

--- shop/core/admin.py
+++ shop/core/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from .models import User,Profile, Categories, Suppliers, Products, Orders, OrderItems
 # Register your models here.
 
-# @admin.register(Profile)
+
 class ProfileInline(admin.StackedInline):
     model = Profile
 
 admin.site.register(Profile,ProfileInline)
 
-# @admin.register(OrderItems)
+
 class OrderItemsInline(admin.StackedInline):
     model = OrderItems
 
@@ -19,7 +19,7 @@ class UserAdmin(admin.ModelAdmin):
     list_display = (
          'email', 'first_name', 'last_name', 'phone_number', 'date_joined', 'last_login', 'is_active', 'is_staff',
         'is_superuser')
-    inlines = [ProfileInline]
+    # inlines = [ProfileInline]
     list_filter = ('date_joined', 'is_active', 'is_staff', 'is_superuser')
     search_fields = ('username', 'email', 'first_name', 'last_name')
     date_hierarchy = 'date_joined'
@@ -85,6 +85,45 @@ class SupplierAdmin(admin.ModelAdmin):
     search_fields = ('user', 'first_name', 'last_name', 'date_joined')
     date_hierarchy = 'date_joined'
     readonly_fields = ('user', 'date_joined')
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_delete_permission(self, request, obj = None):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_change_permission(self, request, obj = None):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_module_permission(self, request):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_view_permission(self, request, obj = None):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_read_permission(self, request, obj= None):
+        return request.user.is_superuser or request.user.is_staff
+    
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'slug', 'category', 'price', 'is_sale', 'sale_price', 'status',
+        'is_suggestion', 'date_added', 'updated_at'
+    )
+
+    list_filter = (
+        'category', 'is_sale', 'status', 'is_suggestion'
+    )
+
+    date_hierarchy = 'date_added'
+
+    search_fields = (
+        'name', 'slug', 'price', 'is_sale', 'sale_price', 'status'
+    )
+
+    readonly_fields = ('date_added', 'updated_at')
 
     def has_add_permission(self, request):
         return request.user.is_superuser or request.user.is_staff
