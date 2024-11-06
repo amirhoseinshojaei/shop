cdc1a6974aa113f5e36ceaea53fbdc29282d9568
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-11-07 00:22:52 +0330

    register supplier model in panle admin

--- shop/core/admin.py
+++ shop/core/admin.py
@@ -2,17 +2,17 @@ from django.contrib import admin
 from .models import User,Profile, Categories, Suppliers, Products, Orders, OrderItems
 # Register your models here.
 
-@admin.register(Profile)
+# @admin.register(Profile)
 class ProfileInline(admin.StackedInline):
     model = Profile
 
+admin.site.register(Profile,ProfileInline)
 
-
-@admin.register(OrderItems)
+# @admin.register(OrderItems)
 class OrderItemsInline(admin.StackedInline):
     model = OrderItems
 
-
+admin.register(OrderItems)
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
@@ -65,6 +65,37 @@ class CategoriesAdmin(admin.ModelAdmin):
         return request.user.is_superuser or request.user.is_staff
     
     def has_module_permission(self, request, obj= None):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_view_permission(self, request, obj = None):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_read_permission(self, request, obj= None):
+        return request.user.is_superuser or request.user.is_staff
+    
+
+
+@admin.register(Suppliers)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 'first_name', 'last_name', 'date_joined'
+    )
+
+    list_filter = ('date_joined',)
+    search_fields = ('user', 'first_name', 'last_name', 'date_joined')
+    date_hierarchy = 'date_joined'
+    readonly_fields = ('user', 'date_joined')
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_delete_permission(self, request, obj = None):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_change_permission(self, request, obj = None):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_module_permission(self, request):
         return request.user.is_superuser or request.user.is_staff
     
     def has_view_permission(self, request, obj = None):
