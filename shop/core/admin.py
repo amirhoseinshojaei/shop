d9e83196ca4c3e78ccb54b4948b55d8ee9b2abfe
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-11-07 00:06:20 +0330

    register categories model in panle admin

--- shop/core/admin.py
+++ shop/core/admin.py
@@ -8,6 +8,12 @@ class ProfileInline(admin.StackedInline):
 
 
 
+@admin.register(OrderItems)
+class OrderItemsInline(admin.StackedInline):
+    model = OrderItems
+
+
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
@@ -36,3 +42,34 @@ class UserAdmin(admin.ModelAdmin):
 
     def has_view_permission(self, request, obj=None):
         return request.user.is_superuser or request.user.is_staff
+    
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'slug', 'date_joined', 'updated_at'
+    )
+
+    list_filter = ('date_joined', 'updated_at')
+    search_fields = ('name', 'slug')
+    date_hierarchy = 'date_joined'
+    readonly_fields = ('date_joined',)
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_delete_permission(self, request, obj = None):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_change_permission(self, request, obj = None):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_module_permission(self, request, obj= None):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_view_permission(self, request, obj = None):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_read_permission(self, request, obj= None):
+        return request.user.is_superuser or request.user.is_staff
+        
