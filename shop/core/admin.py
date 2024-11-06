ff4a8bc22993d5517eb29d4d563d6f8c395b7f32
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-11-07 00:54:35 +0330

    register Orders model in panel admin

--- shop/core/admin.py
+++ shop/core/admin.py
@@ -19,7 +19,7 @@ class UserAdmin(admin.ModelAdmin):
     list_display = (
          'email', 'first_name', 'last_name', 'phone_number', 'date_joined', 'last_login', 'is_active', 'is_staff',
         'is_superuser')
-    # inlines = [ProfileInline]
+    inlines = [ProfileInline]
     list_filter = ('date_joined', 'is_active', 'is_staff', 'is_superuser')
     search_fields = ('username', 'email', 'first_name', 'last_name')
     date_hierarchy = 'date_joined'
@@ -130,6 +130,41 @@ class ProductsAdmin(admin.ModelAdmin):
     
     def has_delete_permission(self, request, obj = None):
         return request.user.is_superuser or request.user.is_staff
+    
+    def has_change_permission(self, request, obj = None):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_module_permission(self, request):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_view_permission(self, request, obj = None):
+        return request.user.is_superuser or request.user.is_staff
+    
+    def has_read_permission(self, request, obj= None):
+        return request.user.is_superuser or request.user.is_staff
+    
+
+
+@admin.register(Orders)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 'fullname', 'city', 'shipping_address', 'amount_paid', 'shipped',
+        'date_shipped', 'status'
+    )
+
+    inlines = [OrderItemsInline]
+    list_filter = (
+        'city', 'shipped', 'status'
+    )
+
+    search_fields = ('fullname', 'city', 'shipping_address', 'status')
+    readonly_fields = ('date_shipped')
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+    
+    def has_delete_permission(self, request, obj = None):
+        return request.user.is_superuser
     
     def has_change_permission(self, request, obj = None):
         return request.user.is_superuser or request.user.is_staff
