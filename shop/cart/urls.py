cb9db62f957fbddb41a034d1432993bd9c707569
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-11-08 21:08:33 +0330

    build a cart add view and add url for cart add view

--- shop/cart/urls.py
+++ shop/cart/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('',views.cart_summary, name='cart_summary'),
-    
+    path('add/', views.cart_add, name='cart_add'),
 ]
--- shop/cart/views.py
+++ shop/cart/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from core.models import Products
-from django.http import HttpResponse
+from core.models import Products
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -19,3 +20,27 @@ def cart_summary(request):
     })
 
 
+def cart_add(request):
+    # Get the cart
+    cart = Cart(request)
+    # Test for POST
+    if request.POST.get('action') == 'post':
+        # Get stuff
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+        # Lookup product in DB
+        product = get_object_or_404(Products, id = product_id)
+        
+        # Save to session
+        cart.add(product=product, quantity=product_qty)
+
+        # Get quantity
+        cart_quantity = cart.__len__()
+        # Return response
+
+        response = JsonResponse({
+            'Product Name': product.name,
+            'qty': cart_quantity
+        })
+
+        return response
