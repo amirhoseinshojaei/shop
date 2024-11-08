354e07722364231a1988ca476e82759f3f7c3777
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-11-08 20:49:15 +0330

    add cart summary view to display products

--- shop/cart/views.py
+++ shop/cart/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from core.models import Products
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def cart_summary(request):
+    # Get the cart
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    totals = cart.cart_total()
+    return render (request, 'cart/cart_summary.html', {
+        'cart_products': cart_products,
+        'quantities' : quantities,
+        'totals': totals
+    })
+
+
