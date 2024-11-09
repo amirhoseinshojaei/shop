d7087e9ed5fec9b670fd394f9f840400c2a67442
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-11-10 01:14:47 +0330

    build home view

--- shop/core/views.py
+++ shop/core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import (User, Profile, Categories, Suppliers
+                     , Products, Orders, OrderItems, ShippingAddress)
 # Create your views here.
+
+
+def index(request):
+    products = Products.objects.filter(status='in_stock')
+    categories = Categories.objects.all()
+
+    context = {
+        'products':products,
+        'categories':categories
+    }
+
+    return render(request,'core/index.html', context)
