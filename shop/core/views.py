0d76e21f552dfadbfdcbfe0272de99d38a4cadf5
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-11-11 15:20:56 +0330

    build view for category details

--- shop/core/views.py
+++ shop/core/views.py
@@ -38,3 +38,17 @@ def categories(request):
     return render(request, 'core/categories.html', context={
         'categories':categories
     })
+
+
+def category_detail(request, slug):
+    try:
+        category = get_object_or_404(Categories, slug=slug)
+        product = Products.objects.filter(category=category)
+        return render(request, 'category_detail.html',{
+            'category':category,
+            'product':product
+        })
+    
+    except Http404:
+        messages.error(request, 'Category does not exist')
+        return render(request, '404.html', status=404)
