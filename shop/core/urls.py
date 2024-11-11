2b226726df4f404fa43b2db5afb259caede4aa17
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-11-11 15:26:33 +0330

    add url for about view

--- shop/core/urls.py
+++ shop/core/urls.py
@@ -7,6 +7,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/',views.about, name='about'),
     path('product/<str:slug>/', views.product_detail, name='product_detail'),
     path('categories/', views.categories, name='categories'),
     path('category/<str:slug>/', views.category_detail, name='category_detail'),
