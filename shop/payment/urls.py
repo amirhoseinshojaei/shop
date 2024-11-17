a40ae102e95b9007c1c1eefcfdda33883df667f4
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-11-17 23:24:57 +0330

    add url path for orders not shipped list view

--- shop/payment/urls.py
+++ shop/payment/urls.py
@@ -7,4 +7,5 @@ app_name = 'payment'
 urlpatterns = [
     path('orders/', views.orders_list, name='orders_list'),
     path('orders/<uuid:id>/', views.orders, name='orders'),
+    path('orders/not_shipped/', views.orders_not_shipped_list, name='orders_not_shipped'),
 ]
