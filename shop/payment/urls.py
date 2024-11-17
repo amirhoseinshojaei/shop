5bcbf285841312b10794b96baa652597f47e9b52
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-11-17 21:42:34 +0330

    add url for orders_list view

--- shop/payment/urls.py
+++ shop/payment/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'payment'
+
 urlpatterns = [
+    path('orders/', views.orders_list, name='orders_list'),
     path('orders/<uuid:id>/', views.orders, name='orders'),
 ]
