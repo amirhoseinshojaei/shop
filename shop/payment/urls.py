7eb1ee0ea22138a8685f2cdd5ec806ccb8a61ce7
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-11-18 20:32:19 +0330

    edit url payment views app

--- shop/payment/urls.py
+++ shop/payment/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'payment'
 
 urlpatterns = [
-    path('orders/', views.orders_list, name='orders_list'),
+    path('orders/', views.dashboard, name='orders_list'),
     path('orders/<uuid:id>/', views.orders, name='orders'),
-    path('orders/not_shipped/', views.orders_not_shipped_list, name='orders_not_shipped'),
+    path('orders/not_shipped/', views.dash_not_shipped_list, name='orders_not_shipped'),
 ]
