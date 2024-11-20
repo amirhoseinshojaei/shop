12c6fe01c1110ea090fc163072af518c1dd7d105
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-11-21 01:37:46 +0330

    build view for order placed and add url for them

--- shop/payment/urls.py
+++ shop/payment/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('orders/', views.dashboard, name='orders_list'),
     path('orders/<uuid:id>/', views.orders, name='orders'),
     path('orders/not_shipped/', views.dash_not_shipped_list, name='orders_not_shipped'),
+    path('payment_form/', views.process_order, name='process_order')
 ]
